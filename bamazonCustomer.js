e29e304429f634f697cffc8e318568ec376202a1
mghoprial <50455586+mghoprial@users.noreply.github.com>
2019-07-19 09:56:17 -0700

    updated the table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-const inquireer = require ('inquirer');
-const mysql = require("mysql2/promise");
+const inquirer = require ('inquirer');
+const mysql = require("mysql2");
 const keys = require ('./keys');
 
 var connection = mysql.createConnection({
@@ -13,4 +13,40 @@ var connection = mysql.createConnection({
 
   database: "bamazon"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
+});
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+     start();
+  });
+  
+  function start() {
+    inquirer
+      .prompt([
+        {
+          name: "idQuestion",
+          type: "input",
+          message: "What is the ID of the product you'd like to purchase?",
+        },
+        {
+          name: "unitQuestion",
+          type: "list",
+          message: "How many units would you like?",
+          choices: [ '0','1', '2', '3', '4']
+        }
+      ]).then(function(answer) {
+        console.log(answer.unitQuestion);
+       
+      });
+  }
+}
 
--- bamazonSeed.sql
+++ bamazonSeed.sql
@@ -2,28 +2,46 @@ DROP DATABASE IF EXISTS bamazon;
 
 CREATE DATABASE bamazon;
 
-USE ice_creamDB;
+USE bamazon;
 
-CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(80) NOT NULL,
-   department_name VARCHAR(80) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  stock_quantity (10) INT NULL,
-  PRIMARY KEY (item_id)
+CREATE TABLE products
+(
+  id INT NOT NULL
+  AUTO_INCREMENT,
+  product_name VARCHAR
+  (80) NOT NULL,
+   department_name VARCHAR
+  (80) NOT NULL,
+  price DECIMAL
+  (10,2) NOT NULL,
+  stock_quantity INT
+  (10)  NULL,
+  PRIMARY KEY
+  (id)
 );
 
-INSERT INTO products
-  (product_name, department_name, price, stock_quantity)
+  INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
 
   VALUES
-  ('transformer bot', 'toys', 6, 10)
-  ('banana', 'food', 2.99, 100)
-  ('headphones', 'electronics', 50, 50)
-  ('tv', 'electronics', 400, 25)
-  ('shirt', 'clothing', 10, 100)
-  ('pull up bar', 'exercise', 30, 45)
-  ('cookies', 'food', 3.99, 200)
-  ('guitar', 'musical instrument', 300, 100)
-  ('toilet', 'bathroom', 200, 20)
-  ('toenail clippers', 'bathroom', 4, 50)
+    ('mp3 player', 'electronics', 6, 10),
+    ('headphones', 'electronics', 50, 50),
+    ('tv', 'electronics', 400, 25),
+    ('shirt', 'clothing', 10, 100),
+    ('pull up bar', 'exercise', 30, 45),
+    ('cookies', 'food', 3.99, 200),
+    ('jeans', 'clothing', 300, 100),
+    ('toilet', 'bathroom', 200, 20),
+    ('toenail clippers', 'bathroom', 4, 50),
+    ('banana', 'food', 3, 100);
+
+
+  -- INSERT INTO departments
+  --   (department_name, over_head_costs)
+  -- VALUES
+  --   ('food', 500),
+  --   ('electronics', 450),
+  --   ('bathroom', 325),
+  --   ('exercise', 500),
+  --   ('clothing', 500)
+
