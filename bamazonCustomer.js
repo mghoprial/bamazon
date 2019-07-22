ca19a2d2f903d9715f28d5b27e4ad60933ffad53
mghoprial <50455586+mghoprial@users.noreply.github.com>
2019-07-22 15:27:03 -0700

    bamazonCustomer shows user which products they bought

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 const mysql = require("mysql2");
-const keys = require ('./keys');
+const keys = require('./keys');
 
 var connection = mysql.createConnection({
   host: "127.0.0.1",
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   readProducts();
@@ -22,31 +22,50 @@ connection.connect(function(err) {
 
 function readProducts() {
   console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-     start();
+    start();
   });
-  
-  function start() {
-    inquirer
-      .prompt([
-        {
-          name: "idQuestion",
-          type: "input",
-          message: "What is the ID of the product you'd like to purchase?",
-        },
-        {
-          name: "unitQuestion",
-          type: "list",
-          message: "How many units would you like?",
-          choices: [ '0','1', '2', '3', '4']
+}
+
+function start() {
+  inquirer
+    .prompt([
+      {
+        name: "idQuestion",
+        type: "input",
+        message: "What is the ID of the product you'd like to purchase?",
+      },
+      {
+        name: "unitQuestion",
+        type: "list",
+        message: "How many units would you like?",
+        choices: ['0', '1', '2', '3', '4']
+      }
+    ]).then(function (answer) {
+      console.log("ordering " + answer.unitQuestion + " unit(s)");
+      // select stock quantity from our products table
+      connection.query(`SELECT * FROM products WHERE ID =${answer.idQuestion}`, function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        var itemInStock = res[0].stock_quantity;
+        // console.log(itemInStock);
+        // check if item is in stock
+        // if not, run message saying "not enough units available"
+        if (answer.unitQuestion > itemInStock) {
+          console.log("not enough units!")
         }
-      ]).then(function(answer) {
-        console.log(answer.unitQuestion);
-       
+        else {
+          // if so, subtract number of units purchased from stock_quantity
+          var newResult = itemInStock - answer.unitQuestion;
+          connection.query(`UPDATE products SET stock_quantity = ${newResult} WHERE ID=${answer.idQuestion}`)
+          console.log("you purchased an " + res[0].product_name)
+        }
+
       });
-  }
+
+    })
 }
 
